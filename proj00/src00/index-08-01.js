7d2fe243757370b500680b0dc2596af977fee167
Laurence2013 <lozza244@protonmail.com>
2025-03-08 16:26:11 +0000

    17-commit

--- proj00/src00/index-08-01.js
+++ proj00/src00/index-08-01.js
@@ -8,33 +8,7 @@
 // @flow
 const { Map, List, fromJS } = require('immutable');
 
-// Flowjs-Interface-00, type declaration and Immutable.js Map()
-interface Address00 {
-	no: number;
-	address: string;
-	postcode: string;
-};
-interface Person00 {
-	id: number;
-	name: string;
-	age?: number;
-	email?: string;
-	address: Address00;
-};
-type personDetails00 = Map<$Keys<Person00>, $Values<Person00>>;
-
-const person00: personDetails00 = Map({
-	id: 00,
-	name: 'Lozza Garcia',
-	age: 29,
-	address: {no: 19, address: 'some secret address 1', postcode: 'wvSomething'}
-});
-const getName00 = person00.get('name') ?? null;
-const getEmail00 = person00.get('email') ?? null;
-const getAddress00 = person00.get('address') ?? null;
-// console.log(getAddress00);
-
-// Flowjs-Interface-01, type declaration and Immutable.js List()
+// Flowjs-Interface-00, type declaration and Immutable.js List()
 interface Address01 {
 	no: number;
 	address: string;
@@ -64,9 +38,19 @@ const person03: Person01 = {
 	address: {no: 20, address: 'some secret address 2', postcode: 'wvSomething'}
 };
 const personList00:person01 = List([person02, person03]);
-// const invalid00:person01 = List([person02, 123]); Error 123
-// const invalid01:person01 = List(['string']); Error 'string'
-
-const getPerson00 = personList00.get(0) ?? null;
+const person04: Person01 = {
+  id: 3,
+  name: 'Lozza',
+  email: 'lozza@example.com',
+	address: {no: 21, address: 'some secret address 3', postcode: 'wvSomething'}
+};
+const getPerson00 = personList00.get(1) ?? null;
 const getName01 = personList00.get(0) ?? null;
-// console.log(getName01.name);
+
+const test00 = personList00.push(person04);
+
+const getPerson01 = test00.get(2) ?? null;
+const getPerson02 = test00.toArray() ?? null;
+const result00 = getPerson02.filter(obj => obj.id === 2)
+
+console.log(result00);
