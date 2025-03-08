ab0cecfb101c33c89e180fd40dc1f5c463fc3c8b
Laurence2013 <lozza244@protonmail.com>
2025-03-08 14:05:10 +0000

    15-commit

--- proj00/src00/index-08.js
+++ proj00/src00/index-08.js
@@ -1,38 +1,50 @@
 /*
-	desc-00: Give code examples using RxJs expand() operator and Immutable Map() object.
-	desc-00a: immutable-map, rxjs-iif, method-signatures
-	desc-01: Example 1:  Hierarchical Data Traversal (Tree Structure)
+	desc-00: 
+	desc-01: 
 	goal:
 	line-code-added:
 */
 // @flow
-const { of, from, interval, EMPTY } = require('rxjs');
-const { tap, map, filter, take, expand, mergeMap, flatMap } = require('rxjs/operators');
 const { Map, List, fromJS } = require('immutable');
 
 // desc-01
-type TreeNode = Map<string, string | number | List<TreeNode>>;
+// Flowjs-Interface-00
+interface MyMapInterface {
+	id: number;
+	fName: string;
+	lName: string;
+	age: number;
+	hobbies: Array<string>;
+};
+type MyMapInterface00 = Map<$Keys<MyMapInterface>, $Values<MyMapInterface>>;
 
-const data00:TreeNode = fromJS({
+const myMap00: MyMapInterface00 = Map(({
 	id: 1,
-	name: 'Root',
-	children: [
-		{ id: 2, name: 'Child A', children: [] },
-		{
-			id: 3,
-			name: 'Child B',
-			children: [{ id: 4, name: 'Grandchild B1', children: [] }],
-		},
-	],
+	fName: 'Lozza',
+	lName: 'Mitchell',
+	age: 29,
+	hobbies: ['Weights', 'Boxing']
+}));
+
+// Flowjs-Interface-01
+interface Address {
+	street: string;
+	city: string
+};
+interface Person {
+	name: string;
+	age: number;
+	address: Address;
+};
+type PersonMapType = Map<$Keys<Person>, $Values<Person>>;
+
+const myMap01: PersonMapType = Map({
+	name: 'Lozza Garcia',
+	age: 29,
+	address: {street: 'Some street 1', city: 'London'}
 });
 
-const result00$ = from(data00).pipe(
-	//map(obj00 => obj00.has('children')),
-	//tap(tst00 => console.log(tst00)),
-	expand(obj99 => obj99.has('children') ? of({name: 'Hello'}) : of({name: 'Nothing'})),
-	tap(tst00 => console.log(tst00)),
-	mergeMap(obj98 => of(obj98).pipe(
-		map(obj97 => obj97.name)
-	))
-);
-result00$.subscribe();
+const name = myMap01.get('name') ?? '';
+const address = myMap01.get('address');
+console.log(address);
+
