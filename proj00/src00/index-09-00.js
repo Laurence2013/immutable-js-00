77da85c7685355a343eb13f7ab51636612c46316
Laurence2013 <lozza244@protonmail.com>
2025-03-11 11:57:54 +0000

    22-commit

--- proj00/src00/index-09-00.js
+++ proj00/src00/index-09-00.js
@@ -11,38 +11,26 @@ const { tap, map, filter, take, expand, mergeMap, flatMap } = require('rxjs/oper
 const { Map, List, fromJS } = require('immutable');
 
 // desc-01
-/*interface Children00 {
-	id: number;
-	name: string;
-	children: Array<Map<string, number | string | [] >>;
-}
-interface User00 {
-	id: number;
-	name: string;
-	children: Array<Map<string, number | string | [] | Map<string, number | string | []>>>;
-};
-type userChildren00 = Map<$Keys<User00>, $Values<User00>>;*/
-
-/*const data01:userChildren00 = Map({
+const data00 = Map({
 	id: 1,
 	name: 'Root',
 	children: [
-		Map({ id: 2, name: 'Child A', children: []}),
-		Map({
+		{ id: 2, name: 'Child A', children: [] },
+		{
 			id: 3,
 			name: 'Child B',
-			children: [Map({ id: 4, name: 'Grandchild B1', children: [] })],
-		}),
+			children: [{ id: 4, name: 'Grandchild B1', children: [] }],
+		},
 	],
 });
-
-const result00$ = from([data01]).pipe(
+const result00$ = from([data00]).pipe(
 	expand(obj99 => {
-		const children = obj99.get('children');
+		const obj98 = fromJS(obj99);
+		const children = obj98.get('children');
 		return children && children.length > 0 ? from(children) : EMPTY;
 	}),
 	mergeMap(obj98 => from([obj98]).pipe(
-		map(obj97 => obj97.get('name'))
+		map(obj97 => obj97.id)
 	))
 );
-result00$.subscribe(console.log);*/
+result00$.subscribe(console.log);
