d922ce85b63dc0fc6e54c4b83f21e94837a34cba
Laurence2013 <lozza244@protonmail.com>
2025-03-16 09:59:16 +0000

    27-commit

--- proj00/src00/index-09-00b.js
+++ proj00/src00/index-09-00b.js
@@ -5,6 +5,7 @@
 	goal:
 	line-code-added:
 */
+// @flow
 const { of, from, interval, EMPTY } = require('rxjs');
 const { tap, map, filter, take, expand, mergeMap, flatMap, reduce } = require('rxjs/operators');
 const { Map, List, fromJS } = require('immutable');
@@ -16,7 +17,7 @@ interface User00 {
 
 type userChildren00 = Map<$Keys<User00>, $Values<User00>>;
 
-const data00 = Map({
+const data00 = {
 	children: [
 		{ id: 2, name: 'Child A', children: [''] },
 		{
@@ -25,19 +26,9 @@ const data00 = Map({
 			children: [{ id: 4, name: 'Grandchild B1', children: [''] }],
 		},
 	],
-});
-console.log('1. data00: ', data00);
-console.log('2. data00: ', data00.get('children'));
-const test00 = data00.get('children');
-//console.log('test00: ', test00);
-const test01a = test00.map((val, idx) => [Map(val)]);
-console.log('1. test01a: ', test01a);
-//console.log('2. test01a: ', test01a[0]);
-test01a.map(obj00 => obj00.map(obj01 => console.log('3. test01a: ', obj01.get('name'))));
+};
+const test00 = data00.children; // Of type normal JS array, normal JS map
 const test01 = test00.map((val, idx) => [val]);
-const test02 = test01.map(obj => {
-	const test03:Map<string, mixed> = Map(obj[0]);
-	return test03;
-});
-//test02.map(obj => console.log(obj.get('name')));
+const test02 = test01.map(obj00 => obj00.map(obj01 => obj01.name));
+console.log(test02);
 
--- proj00/src00/index-09-00c.js
+++ proj00/src00/index-09-00c.js
@@ -5,6 +5,7 @@
 	goal:
 	line-code-added:
 */
+// @flow
 const { of, from, interval, EMPTY } = require('rxjs');
 const { tap, map, filter, take, expand, mergeMap, flatMap, reduce } = require('rxjs/operators');
 const { Map, List, fromJS } = require('immutable');
@@ -26,18 +27,9 @@ const data00 = Map({
 		},
 	],
 });
-console.log('1. data00: ', data00);
-console.log('2. data00: ', data00.get('children'));
-const test00 = data00.get('children');
-//console.log('test00: ', test00);
-const test01a = test00.map((val, idx) => [Map(val)]);
-console.log('1. test01a: ', test01a);
-//console.log('2. test01a: ', test01a[0]);
-test01a.map(obj00 => obj00.map(obj01 => console.log('3. test01a: ', obj01.get('name'))));
-const test01 = test00.map((val, idx) => [val]);
-const test02 = test01.map(obj => {
-	const test03:Map<string, mixed> = Map(obj[0]);
-	return test03;
-});
-//test02.map(obj => console.log(obj.get('name')));
+const test00 = data00.get('children'); // Of type normal JS array, normal JS map
+console.log(test00);
+const test01 = fromJS(test00);
+console.log(test01);
+console.log(test01.toMap());
 
